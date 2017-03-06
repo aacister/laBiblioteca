2f657ea79b2634678caa3cb633351feb88185090
aacister <aacister@gmail.com>
2017-03-06 18:46:21 -0500

    Changed from ws:// to wss:// in config

--- app/config/config.ts
+++ app/config/config.ts
@@ -1,6 +1,6 @@
 export class LaBibliotecaConfig {
 
-    public static get MOVIE_SOCKET(): string{ return 'ws://labibliotecaapi.herokuapp.com/movies';}
-    public static get MUSIC_SOCKET(): string{ return 'ws://labibliotecaapi.herokuapp.com/songs';}
+    public static get MOVIE_SOCKET(): string{ return 'wss://labibliotecaapi.herokuapp.com/movies';}
+    public static get MUSIC_SOCKET(): string{ return 'wss://labibliotecaapi.herokuapp.com/songs';}
 
 }
